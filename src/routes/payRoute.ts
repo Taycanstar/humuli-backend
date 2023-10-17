672f2376a5698534b9c1f02d95a01ae28f926e62
Taycanstar <diminunez06@gmail.com>
2023-10-16 20:19:27 -0400

    make it better

--- src/routes/payRoute.ts
+++ src/routes/payRoute.ts
@@ -4,5 +4,6 @@ import { payController } from "../controllers/payController"; // Import the hand
 const router = express.Router();
 
 router.post("/webhook", payController.webhookHandler);
+router.post("/create-checkout-session", payController.createCheckoutSession);
 
 export default router;
