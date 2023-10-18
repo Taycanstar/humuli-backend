1becffb0481f7078dbc006b4b8e7d39464646935
Taycanstar <diminunez06@gmail.com>
2023-10-17 23:56:32 -0400

    make it better

--- src/controllers/payController.js
+++ src/controllers/payController.js
@@ -40,7 +40,7 @@ exports.payController = {
                     },
                 ],
                 mode: "subscription",
-                success_url: `https://maxticker-55df64f66a64.herokuapp.com/success.html`,
+                success_url: `https://maxticker-55df64f66a64.herokuapp.com/public/success`,
                 cancel_url: `${req.protocol}://${req.get("host")}/cancel`,
                 metadata: {
                     userId: userId, // Include userId in metadata
