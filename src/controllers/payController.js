e66f0ec76e4e3806075fa31b1a99eb5442078b85
Taycanstar <diminunez06@gmail.com>
2023-10-24 15:59:54 -0400

    make it better

--- src/controllers/payController.js
+++ src/controllers/payController.js
@@ -16,7 +16,6 @@ exports.payController = void 0;
 const stripe_1 = require("stripe");
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-const User_1 = __importDefault(require("../models/User"));
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, "../../.env") });
 const stripeInstance = new stripe_1.Stripe(process.env.STRIPE_KEY_TEST, {
     apiVersion: "2023-08-16",
@@ -52,56 +51,5 @@ exports.payController = {
             res.status(500).send(error.message);
             console.log(error, "error here");
         }
-    }),
-    webhookHandler: (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        let data = "";
-        req.setEncoding("utf8");
-        req.on("data", (chunk) => {
-            data += chunk;
-        });
-        req.on("end", () => __awaiter(void 0, void 0, void 0, function* () {
-            var _a;
-            const sigHeader = req.headers["stripe-signature"];
-            const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET_TEST;
-            if (!sigHeader) {
-                console.error("Webhook Error: Invalid signature header");
-                return; // You might want to log this situation
-            }
-            let event;
-            try {
-                event = stripeInstance.webhooks.constructEvent(data, sigHeader, endpointSecret);
-                res.status(200).send("Received"); // Acknowledge receipt of the event immediately
-            }
-            catch (err) {
-                console.error("Webhook Error:", err.message);
-                return; // You might want to log this situation
-            }
-            console.log("✅ Success:", event.id);
-            if (event.type === "checkout.session.completed") {
-                const session = event.data.object;
-                const userId = (_a = session === null || session === void 0 ? void 0 : session.metadata) === null || _a === void 0 ? void 0 : _a.userId;
-                console.log("Event received:", event);
-                console.log("User ID:", userId);
-                if (!userId) {
-                    console.error("Webhook Error: User ID not found");
-                    return; // You might want to log this situation
-                }
-                try {
-                    const user = yield User_1.default.findByIdAndUpdate(userId, { subscription: "plus" }, { new: true });
-                    if (!user) {
-                        console.error("User not found with ID:", userId);
-                        return; // You might want to log this situation
-                    }
-                    console.log("User update result:", user);
-                }
-                catch (updateErr) {
-                    console.error("Database Update Error:", updateErr);
-                    return; // You might want to log this situation
-                }
-                return;
-            }
-            // Optionally handle other event types
-            console.log("Unhandled event type");
-        }));
     }),
 };
--- src/routes/payRoute.js
+++ src/routes/payRoute.js
@@ -6,6 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const payController_1 = require("../controllers/payController"); // Import the handler function
 const router = express_1.default.Router();
-router.post("/webhook", payController_1.payController.webhookHandler);
 router.post("/create-checkout-session", payController_1.payController.createCheckoutSession);
 exports.default = router;
