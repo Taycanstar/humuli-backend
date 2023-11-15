b6d19b6b2792eeab8ce1b5f5311792e676e8d52b
Taycanstar <diminunez06@gmail.com>
2023-11-15 01:18:04 -0500

    make it better

--- src/routes/apiRoute.ts
+++ src/routes/apiRoute.ts
@@ -4,7 +4,7 @@ import { requireLogin } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/get-users", apiController.getUsers);
-router.get("/get-user-by-id/:id", requireLogin, apiController.getUserById);
+router.get("/get-user-by-id/:id", apiController.getUserById);
 router.get("/get-user", apiController.getUser);
 router.put("/update-user", requireLogin, apiController.updateUser);
 router.get("/get-user-by-value", apiController.getUserByValue);
