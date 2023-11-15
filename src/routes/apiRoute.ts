af46076c7dcc76afb8a7aea337395eae00ded5bd
Taycanstar <diminunez06@gmail.com>
2023-11-15 01:19:56 -0500

    make it better

--- src/routes/apiRoute.ts
+++ src/routes/apiRoute.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/get-users", apiController.getUsers);
 router.get("/get-user-by-id/:id", apiController.getUserById);
 router.get("/get-user", apiController.getUser);
-router.put("/update-user", requireLogin, apiController.updateUser);
+router.put("/update-user", apiController.updateUser);
 router.get("/get-user-by-value", apiController.getUserByValue);
 
 export default router;
